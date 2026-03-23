c4c254474c4ee2a7e7c3a37d206f763de8318567
ErBW_s <erbws@foxmail.com>
2026-03-23 17:54:42 +0800

    Change tar format to ustar for compression

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-tar --no-mac-metadata --no-xattrs -czf ohos-sdk-linux-amd64.tar.gz command-line-tools
+tar --format=ustar --no-mac-metadata --no-xattrs -czf ohos-sdk-linux-amd64.tar.gz command-line-tools
 sha256sum ohos-sdk-linux-amd64.tar.gz > ohos-sdk-linux-amd64.tar.gz.sha256
 split -b 1G ohos-sdk-linux-amd64.tar.gz ohos-sdk-linux-amd64.tar.gz.
